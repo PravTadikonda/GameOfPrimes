dad7201f6ff432e5a76696d680242eea51d0b4e1
PravTadikonda <pravtadikonda410@yahoo.com>
2015-07-08 03:25:17 -0400

    jialefjlsd

---
+++ TeamNameHi.m
@@ -0,0 +1,15 @@
+classdef TeamNameHi
+    %TEAMNAMEHI Summary of this class goes here
+    %   Detailed explanation goes here
+    sum = 0
+    if 1 < 100
+       sum = 70 
+    end
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
