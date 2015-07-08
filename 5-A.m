c8b8a5d5bb41be7a6ea44dd293ea4ecda7dd7f94
PravTadikonda <pravtadikonda410@yahoo.com>
2015-07-08 04:01:46 -0400

    First Computational Model

---
+++ 5-A.m
@@ -0,0 +1,20 @@
+classdef 5-A
+    function solution = N_A(cardNums, targetNum)
+    % N_A  Finds a solution for a single hand of the game of Primes.
+    %   SOLUTION = N_A(CARDNUMS, TARGETNUM) returns a string description
+    %   of a solution to the given Primes problem or an empty string if no
+    %   solution is found, where the problem is defined by a vector of six
+    %   card numbers CARDNUMS and a single target number TARGETNUM.
+    %
+    %   For example, N_A([4 9 13 1 4 2], 157) might return the string
+    %   '9 * 13 + 4 * 2 * (4+1)'.
+    %
+    %   <ANY ADDITIONAL DOCUMENTATION>
+    %
+    %   by <TEAM MEMBER NAMES>
+    %   Last updated <DATE>
+
+
+    end  % end of function
+end
+
