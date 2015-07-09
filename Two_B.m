4fec9fee514e2f71fbb59186cd8461e278c6f35a
PravTadikonda <pravtadikonda410@yahoo.com>
2015-07-09 19:49:56 -0400

    adding a new one

---
+++ Two_B.m
@@ -0,0 +1,18 @@
+function [ output_args ] = Two_B( input_args )
+% Finds a solution for a single hand of the game of Primes.
+%   SOLUTION = N_A(CARDNUMS, TARGETNUM) returns a string description
+%   of a solution to the given Primes problem or an empty string if no
+%   solution is found, where the problem is defined by a vector of six
+%   card numbers CARDNUMS and a single target number TARGETNUM.
+%
+%   For example, N_A([4 9 13 1 4 2], 157) might return the string
+%   '9 * 13 + 4 * 2 * (4+1)'.
+%
+%   <ANY ADDITIONAL DOCUMENTATION>
+%
+%   by Kimberly Wolford, D'Miria Collins, Prav Tadikonda
+%   Last updated <DATE>
+
+
+end
+
