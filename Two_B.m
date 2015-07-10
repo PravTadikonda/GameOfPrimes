0f3c99ab149aeb4d5ad18ef2e292e4a0d80ac1dd
PravTadikonda <pravtadikonda410@yahoo.com>
2015-07-10 19:17:13 -0400

    testing

--- Two_B.m
+++ Two_B.m
@@ -1,4 +1,4 @@
-function [ output_args ] = Two_B( input_args )
+function solution = Two_B(cardNums, targetNum)
 % Finds a solution for a single hand of the game of Primes.
 %   SOLUTION = N_A(CARDNUMS, TARGETNUM) returns a string description
 %   of a solution to the given Primes problem or an empty string if no
@@ -12,6 +12,23 @@ function [ output_args ] = Two_B( input_args )
 %
 %   by Kimberly Wolford, D'Miria Collins, Prav Tadikonda
 %   Last updated <DATE>
+
+
+% ask user for a number
+prompt = 'Choose a number from 100 - 200 that''s relatively prime\n';
+primeNum = input(prompt);
+    % check if it's about prime (not divisible by 2 or 5)
+    while (mod(primeNum,2) == 0 | mod(primeNum, 5) == 0)
+        prompt2 = 'Sorry, your number is obviously a composite number. Try again, please.\n';
+        primeNum = input(prompt2);
+    end
+%display(primeNum);
+        
+% randomly choose 6 different numbers
+
+
+
+
 
 
 end
