599ae330cb290badcc11976120383e455b740587
PravTadikonda <pravtadikonda410@yahoo.com>
2015-07-14 21:58:27 -0400

    Just added intro

--- Two_B.m
+++ Two_B.m
@@ -15,20 +15,52 @@ function solution = Two_B(cardNums, targetNum)
 
 
 % ask user for a number
-prompt = 'Choose a number from 100 - 200 that''s relatively prime\n';
+prompt = 'Choose a number from 100 to 200 that''s relatively prime\n';
 primeNum = input(prompt);
-    % check if it's about prime (not divisible by 2 or 5)
-    while (mod(primeNum,2) == 0 | mod(primeNum, 5) == 0)
-        prompt2 = 'Sorry, your number is obviously a composite number. Try again, please.\n';
+    while (~isprime(primeNum) || primeNum < 100 || primeNum > 200)
+        if (primeNum > 200)
+            display('Whoops! The number is too large!')
+        else if (primeNum < 100)
+            display('Whoops! The number is too small!')
+        else if (~isprime(primeNum))
+            display('Whoops! You picked a composite number!')
+            end
+            end
+        end
+        prompt2 = 'Try again, please.\n';
         primeNum = input(prompt2);
     end
-%display(primeNum);
-        
-% randomly choose 6 different numbers
 
+% randomly choose 6 different numbers and makes sure it works
+dealAgain = true;
+while(dealAgain)
+    randNums = randperm(52,6);
+    cards = [];
+    for x = 1:numel(randNums)
+        card = mod(randNums(x),13);
+        if card == 0
+            card = 13;
+        end
+        cards(x) = card;
+    end
 
+    display('These are the cards you are dealt:');
+    disp(cards);
 
+    %check if it's possible
+    total = 1;
+    for x = 1:numel(cards)
+        total = total * cards(x);
+    end
+    if total < primeNum
+        disp('This will not be possible. We will deal again.');
+        dealAgain = true;
+    else
+        dealAgain = false;
+    end
+end
 
+% Use brute force to check
 
 
 end
