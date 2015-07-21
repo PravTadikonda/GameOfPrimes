0eba4b7fa3f1386c6cb21356edcd14fc60adac3e
kwolford3 <kwolford3@gatech.edu>
2015-07-21 06:26:10 -0400

    Took out user input and card choosing

--- Two_B.m
+++ Two_B.m
@@ -13,57 +13,11 @@ function solution = Two_B(cardNums, targetNum)
 %   by Kimberly Wolford, D'Miria Collins, Prav Tadikonda
 %   Last updated 07/15/2015
 
-
-% ask user for a number
-prompt = 'Choose a number from 100 to 200 that''s relatively prime\n';
-primeNum = input(prompt);
-    while (~isprime(primeNum) || primeNum < 100 || primeNum > 200)
-        if (primeNum > 200)
-            display('Whoops! The number is too large!')
-        else if (primeNum < 100)
-            display('Whoops! The number is too small!')
-        else if (~isprime(primeNum))
-            display('Whoops! You picked a composite number!')
-            end
-            end
-        end
-        prompt2 = 'Try again, please.\n';
-        primeNum = input(prompt2);
-    end
-
-% randomly choose 6 different numbers and makes sure it works
-dealAgain = true;
-while(dealAgain)
-    randNums = randperm(52,6);
-    cards = [];
-    for x = 1:numel(randNums)
-        card = mod(randNums(x),13);
-        if card == 0
-            card = 13;
-        end
-        cards(x) = card;
-    end
-
-    display('These are the cards you are dealt:');
-    disp(cards);
-
-    %check if it's possible
-    total = 1;
-    for x = 1:numel(cards)
-        total = total * cards(x);
-    end
-    if total < primeNum
-        disp('This will not be possible. We will deal again.');
-        dealAgain = true;
-    else
-        dealAgain = false;
-    end
-end
-
 % Use brute force to check
 tic;
+primeNum = targetNum;
 %get all the numbers (6! ways)
-allPossibleNumOrder = perms(cards);
+allPossibleNumOrder = perms(cardNums);
 totalNumCombos = size(allPossibleNumOrder,1);
 
 %get all the operations (4^5 ways)
